c1dde068a0a2be2f110cb8ccfae5c501e16ca78f
s28629288@gmail.com <s28629288@gmail.com>
2019-07-22 04:18:28 +0800

    first commit !

---
+++ ex0101.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# ex0101.py
+# @Author :  ()
+# @Link   : 
+# @Date   : 2019/7/22 ä¸????4:16:11
+print("ok")
---
+++ ex0102.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# ex0102.py
+# @Author :  ()
+# @Link   : 
+# @Date   : 2019/7/22 ä¸????4:16:50
+print("ok_1")
